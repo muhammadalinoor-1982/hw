f3de7dfb2df4a973389a270f3e5f1a26a6c3e3ed
muhammadalinoor-1982 <noor.web.journey@gmail.com>
2023-05-30 23:41:44 +0600

    Read operation and create from design 29/05/2023

--- App/urls.py
+++ App/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', main, name='main'),
+    path('create/', create, name='create')
 ]
--- App/views.py
+++ App/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def main(Request):
-    value = Request.POST.get('txt')
-    # print(value)
-    context = {
-        'data' : value
-    }
-    return render(Request,'First.html', context)
+    user_prof = Profile.objects.all()
+    return render(Request,'First.html', locals())
+
+def create(Request):
+    return render(Request, 'create.html', locals())
 
 
